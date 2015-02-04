c2f584ab353f4d699b7cffdef297ba62b7d85534
Ralph <ralph.waldenmaier@gmail.com>
2015-02-04 22:47:23 +0100

    adding plot2

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+
 if (!require("data.table")) {
   install.packages("data.table")
 }
@@ -27,7 +28,7 @@ if(!file.exists(unzippedFile))
   print(zipfile)
   print(unzippedFile)
   unzip(zipfile, files = unzippedFile, list = FALSE, overwrite = TRUE,
-          junkpaths = TRUE, exdir = ".", unzip = "internal",setTimes = FALSE)  
+        junkpaths = TRUE, exdir = ".", unzip = "internal",setTimes = FALSE)  
 }
 
 ## reading data
@@ -40,9 +41,18 @@ data$Date <- as.Date(data$Date, "%d/%m/%Y")
 validDates <- c(as.Date("01/02/2007", "%d/%m/%Y"), as.Date("02/02/2007", "%d/%m/%Y"))
 data.filtered <- filter(data, Date %in% validDates)
 data.filtered$Global_active_power <- as.numeric(data.filtered$Global_active_power)
+#data.filtered <- transform(data.filtered, dow=as.factor(format(data.filtered$Date, format="%a")))
+data.filtered <- transform(data.filtered, 
+                           tm2=strptime(paste(data.filtered$Date, data.filtered$Time), format="%Y-%m-%d %H:%M:%S"))
 
 ## creating plot1
 print("creating plot1.png")
-png(file ="plot1.png", bg="white", width=480, height=480)
-with(data.filtered, hist(Global_active_power, main ="Global Active Power", col="orangered", xlab="Global Active Power (kilowatts)"))
+
+png(file ="plot2.png", bg="white", width=480, height=480)
+par(mar = c(3,4,4,2))
+##with(data.filtered, hist(Global_active_power, dow, main ="Global Active Power", col="orangered", xlab="Global Active Power (kilowatts)"))
+with(data.filtered, plot(Global_active_power ~ tm2, main ="Global Active Power", col="black", ylab="Global Active Power (kilowatts)", type="l"))
 dev.off()
+
+
+as.factor(format(data.filtered$Date, format="%a"))
